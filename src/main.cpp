3d48c148a1c88a533e30a69636609addc9a6907b
Elias Daler <eliasdaler@yandex.ru>
2022-04-17 18:06:25 +0300

    Make default window a bit bigger

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "imgui.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
